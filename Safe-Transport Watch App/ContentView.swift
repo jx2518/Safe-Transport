4d61dc0531bdb3825c70219e886824a0a6dac341
Jingpu Xu <jx2518@columbia.edu>
2023-10-09 00:13:41 -0400

    Initial Commit

---
+++ Safe-Transport Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Safe-Transport Watch App
+//
+//  Created by  jx2518 on 10/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Safe-Transport Watch App/Safe_TransportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safe_TransportApp.swift
+//  Safe-Transport Watch App
+//
+//  Created by  jx2518 on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Safe_Transport_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
